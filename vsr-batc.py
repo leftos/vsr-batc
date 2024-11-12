fecaa8ff82193695bba9740247bcc65b77029610
Leftos Aslanoglou <leftos@gmail.com>
2024-11-12 07:58:59 -0800

    Add less compact mode, make the network identifier smaller and a different color, improve callsign detection logic

--- vsr-batc.py
+++ vsr-batc.py
@@ -45,7 +45,7 @@ json_data = """
         "From": "Unknown"
     },
     "Metadata": {
-        "OriginatingNetwork": "BATC",
+        "OriginatingNetwork": "BATC</span>",
         "OriginatingNetworkUserIdentifier": "",
         "MessageClass": 22
     },
@@ -67,6 +67,8 @@ last_line_index = -1
 
 include_user_initiated = False
 
+less_compact = False
+
 # Set to the equivalent of %userprofile%\AppData\LocalLow\Skirmish Mode Games, Inc\BeyondATC\Player.log for the current user
 log_path = os.path.expanduser("~") + "\\AppData\\LocalLow\\Skirmish Mode Games, Inc\\BeyondATC\\Player.log"
 
@@ -75,6 +77,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--playback_emulation", help="Enable playback emulation mode", action="store_true")
 parser.add_argument("-u", "--include_user_initiated", help="Include user initiated messages", action="store_true")
 parser.add_argument("-l", "--log_path", help="Path to the player.log file", default=log_path)
+parser.add_argument("-lc", "--less_compact", help="Use less information dense mode", action="store_true")
 args = parser.parse_args()
 
 if args.playback_emulation:
@@ -82,6 +85,9 @@ if args.playback_emulation:
     
 if args.include_user_initiated:
     include_user_initiated = True
+    
+if args.less_compact:
+    less_compact = True
     
 log_path = args.log_path
 
@@ -96,6 +102,14 @@ print("You can minimize this window, but do not close it.")
 print("This integration will run in the background without need for interaction from you.")
 print("Make sure the ATC App Messages filter is enabled in VSR's settings.")
 print("=====================================")
+
+def is_likely_callsign(string):
+    # if the string is at least 2 words long and the last word is a number, it's likely a callsign
+    if (len(string.split(' ')) >= 2 and string.split(' ')[-1].isnumeric()):
+        return True
+    # if the string is 5 to 6 characters, all caps, optionally with a dash somewhere in the middle, it's likely a callsign
+    if len(string) >= 5 and len(string) <= 6 and string.isupper() and (string.count('-') == 0 or string.count('-') == 1):
+        return True
 
 while True:
     time.sleep(0.5)
@@ -149,30 +163,19 @@ while True:
     
     # Try to determine the user's callsign, by finding the first line that contains the a comma, followed by a name that ends in an ATC control identifier,
     # and vice-versa
-    for line in reversed(lines):
-        if "ATIS" in line or "METAR" in line:
-            continue
-        if "," in line:
-            first_part = line.split(",")[0]
-            second_part = line.split(",")[1].split(",")[0]
-            
-            if first_part.startswith("Contact"):
-                continue
-            
-            if any(first_part.endswith(x) for x in atc_identifiers):
-                if second_part.split(' ')[-1].isnumeric():
-                    atc_callsign = first_part
-                    user_callsign = second_part
-                break
-            
-            if second_part.startswith(" contact"):
-                continue
-            
-            if any(second_part.endswith(x) for x in atc_identifiers):
-                if first_part.split(' ')[-1].isnumeric():
-                    user_callsign = first_part
-                    atc_callsign = second_part
-                break
+    if not "ATIS" in cur_atc_line and not "METAR" in cur_atc_line and ',' in cur_atc_line:
+        first_part = cur_atc_line.split(",")[0]
+        second_part = cur_atc_line.split(",")[1].split(",")[0]
+        
+        if not first_part.startswith("Contact") and any(first_part.endswith(x) for x in atc_identifiers):
+            if is_likely_callsign(second_part):
+                atc_callsign = first_part
+                user_callsign = second_part
+        
+        if not second_part.startswith(" contact") and any(second_part.endswith(x) for x in atc_identifiers):
+            if is_likely_callsign(first_part):
+                user_callsign = first_part
+                atc_callsign = second_part
     
     # If the last ATC line is different from the current ATC line, send a message
     if last_atc_line != cur_atc_line:
@@ -189,8 +192,11 @@ while True:
             json_data["Headers"]["From"] = user_callsign
             cur_atc_line = cur_atc_line.replace("Speech Transcription Processed: ", "")
             # Surround the cur_atc_line in an HTML color tag with a light purple color to discern user initiated messages
-            cur_atc_line = f"<font color='#D8BFD8'>{cur_atc_line}</font>"
+            cur_atc_line = f"<span style='color:#D8BFD8'>{cur_atc_line}</font>"
         
+        json_data["Headers"]["From"] += " <span style='font-size: 8px; color:#f5f5f5'>"
+        if less_compact:
+            json_data["Headers"]["From"] += "<br>"
         json_data["MessageContent"]["Text"] = cur_atc_line
         
         # Make the POST request with JSON body
