5986a872d47f3f6b3147f05d3dcd0400b336c133
Leftos Aslanoglou <leftos@gmail.com>
2024-11-14 10:41:51 -0800

    Bump version to 0.2.6

--- vsr-batc.py
+++ vsr-batc.py
@@ -6,7 +6,7 @@ import requests
 import time
 import uuid
 
-version = "0.2.5"
+version = "0.2.6"
 
 # To: isn't used but needs to be set, you can use the user's callsign if you have it, 
 # From is typically the aircraft's or ATC callsign (important for BATC messages).  
